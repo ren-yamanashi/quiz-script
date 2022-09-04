9249039cf881cb48b4702fe8571bef68710a592c
Ren <96362223+ren-yamanashi@users.noreply.github.com>
2022-09-04 19:35:47 +0900

    apiSetting完了 (#2)
    * apiSetting完了
    
    * prettier構成 setting

---
+++ api/client.ts
@@ -0,0 +1,20 @@
+// api/client.ts
+import {
+  ApolloClient,
+  InMemoryCache,
+  DefaultOptions,
+  createHttpLink,
+} from '@apollo/client/core';
+
+const defaultOptions: DefaultOptions = {
+  query: {
+    fetchPolicy: 'network-only',
+  },
+};
+const link = createHttpLink({ uri: 'http://localhost:4000/query' });
+
+export const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+  defaultOptions,
+});
--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -61,4 +61,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-}
+};
